8cd8ea2790b8e6510c1de2242ea1bdb8b338af51
aripalo <ari@aripalo.fi>
2013-11-14 00:41:35 +0200

    better describe text for mocha describe

--- with-casper-mocha-chai.coffee
+++ with-casper-mocha-chai.coffee
@@ -1,13 +1,17 @@
 # run with `mocha-casperjs with-casper-mocha-chai.coffee`
-describe "Google searching", ->
+describe "Searching Google for 'casperjs'", ->
   before ->
     casper.start "http://www.google.com/"
 
   it "should retrieve 10 or more results", ->
+
+    expectedHomePageTitle = "Google"
+    expectedSearchForm = "form[action=\"/search\"]"
+
     casper.then ->
-      "Google".should.matchTitle
-      "form[action=\"/search\"]".should.be.inDOM.and.be.visible
-      @fill( "form[action=\"/search\"]", { q: "casperjs" }, true )
+      expectedHomePageTitle.should.matchTitle
+      expectedSearchForm.should.be.inDOM.and.be.visible
+      @fill( expectedSearchForm, { q: "casperjs" }, true )
 
     casper.waitForUrl /q=casperjs/, ->
       (/casperjs/).should.matchTitle
--- with-casper-mocha-chai.js
+++ with-casper-mocha-chai.js
@@ -1,14 +1,18 @@
 // run with `mocha-casperjs with-casper-mocha-chai.js`
-describe('Google searching', function() {
+describe("Searching Google for 'casperjs'", function() {
   before(function() {
     casper.start('http://www.google.com/')
   })
 
   it('should retrieve 10 or more results', function() {
+
+    var expectedHomePageTitle = "Google";
+    var expectedSearchForm = "form[action=\"/search\"]";
+
     casper.then(function() {
-      'Google'.should.matchTitle
-      'form[action="/search"]'.should.be.inDOM.and.be.visible
-      this.fill( 'form[action="/search"]', { q: 'casperjs' }, true )
+      expectedHomePageTitle.should.matchTitle
+      expectedSearchForm.should.be.inDOM.and.be.visible
+      this.fill( expectedSearchForm, { q: 'casperjs' }, true )
     })
 
     casper.waitForUrl(/q=casperjs/, function() {
